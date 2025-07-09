8e389a69c875cc284efba6fe38584cc982c3c33c
Donny <donnymoving@gmail.com>
2025-07-09 12:14:06 +0800

    新增网格交易策略和移动平均策略实现，包含策略初始化、启动、停止及交易信号生成等功能，同时添加策略工厂测试，演示策略创建和管理流程。

---
+++ src/strategies/grid_trading_strategy.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: Homalos_v2
+@FileName   : grid_trading_strategy
+@Date       : 2025/7/9 12:02
+@Author     : Donny
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: description
+"""
---
+++ test/test_strategy_factory..py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: Homalos_v2
+@FileName   : test_strategy_factory.
+@Date       : 2025/7/9 12:12
+@Author     : Donny
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: description
+"""
