55bd27f88c89dfc3d44b0f43060e1ade381fb126
Donny <donnymoving@gmail.com>
2025-07-09 10:36:21 +0800

    新增事件监控器及其测试文件，包含事件日志、分析、性能和安全监控器的实现，演示监控器的使用场景。

---
+++ test/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: Homalos_v2
+@FileName   : __init__.py
+@Date       : 2025/7/9 10:30
+@Author     : Donny
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: description
+"""
---
+++ test/test_event_monitors.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: Homalos_v2
+@FileName   : test_event_monitors
+@Date       : 2025/7/9 10:31
+@Author     : Donny
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: description
+"""
