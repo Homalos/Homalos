ed13d58531bffa0f9ebd25be23d1eb7a0b7dc4e6
Donny <donnymoving@gmail.com>
2025-07-22 23:10:46 +0800

    refactor(映射): 将交易所映射合并到数据映射文件并添加间隔映射
    将原exchange_mapping.py文件删除，其内容合并到新建的data_mapping.py文件中
    新增INTERVAL_MAPPING用于时间间隔的枚举映射

---
+++ src/function/data_mapping.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: Homalos_v2
+@FileName   : exchange_mapping
+@Date       : 2025/7/18 00:30
+@Author     : Donny
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: 交易所映射
+"""
+from src.config.constant import Exchange, Interval
+
+EXCHANGE_MAPPING: dict[str, Exchange] = {
+    "CZCE": Exchange.CZCE,
+    "SHFE": Exchange.SHFE,
+    "DCE": Exchange.DCE,
+    "CFFEX": Exchange.CFFEX,
+    "INE": Exchange.INE,
+    "GFEX": Exchange.GFEX
+}
+
+INTERVAL_MAPPING: dict[int, Interval] = {
+    1: Interval.MINUTE,
+    3: Interval.THREE_MINUTE,
+    5: Interval.FIVE_MINUTE,
+    8: Interval.EIGHT_MINUTE,
+    13: Interval.THIRTEEN_MINUTE,
+    21: Interval.TWENTY_ONE_MINUTE,
+    34: Interval.THIRTY_FOUR_MINUTE,
+    55: Interval.FIFTY_FIVE_MINUTE,
+    60: Interval.HOUR,
+    89: Interval.EIGHTY_NINE_MINUTE,
+    144: Interval.ONE_HUNDRED_FORTY_FOUR_MINUTE,
+    1440: Interval.DAILY,
+    10080: Interval.WEEKLY
+}
+
