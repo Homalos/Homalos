94091810fd1630873233b2d61246dc9f25f54a93
Donny <donnymoving@gmail.com>
2025-09-06 18:41:41 +0800

    Delete some files.

--- src/ctp/gateway/__init__.py
+++
@@ -1,4 +0,0 @@
-from .market_data_gateway import MarketDataGateway
-from .order_trading_gateway import OrderTradingGateway
-
-__all__ = ["MarketDataGateway", "OrderTradingGateway"]
--- src/tts/api/__init__.py
+++
@@ -1,3 +0,0 @@
-from .tts_constant import *
-from .ttsmd import MdApi
-from .ttstd import TdApi
