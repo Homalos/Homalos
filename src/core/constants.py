7bcbf07535c1073f922ff17c5c9ee4224933a59b
Donny <donnymoving@gmail.com>
2025-09-08 17:50:53 +0800

    feat(core): add constants.py file with enumerations for trading platform
    - Create constants.py file in core module
    - Add various enumeration classes for:
      - Direction (long, short, net)  - Offset (open, close, close_today, close_yesterday) - Product (futures, option)  - OrderType (limit, market, stop, FAK, FOK, RFQ)
      - OptionType (call, put)
      - Exchange (various Chinese exchanges)
      - Currency (CNY)
      - Interval (various time intervals for data)- These enumerations provide standardized constants for use in the trading platform

---
+++ src/core/constants.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: Homalos
+@FileName   : constants.py
+@Date       : 2025/9/8 17:37
+@Author     : Lumosylva
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: 交易平台中使用的通用常量枚举。
+
+General constant enums used in the trading platform.
+"""
+from enum import Enum
+
+
+class Direction(Enum):
+    """
+    订单/交易/仓位的方向
+
+    Direction of order/trade/position
+    """
+    LONG = "long"  # 多
+    SHORT = "short"  # 空
+    NET = "net"  # 净
+
+
+class Offset(Enum):
+    """
+    订单的开平仓方向
+
+    Offset of order/trade
+    """
+    NONE = ""                           # 无
+    OPEN = "open"                       # 开
+    CLOSE = "close"                     # 平
+    CLOSE_TODAY = "close_today"         # 平今
+    CLOSE_YESTERDAY = "close_yesterday" # 平昨
+
+
+class Product(Enum):
+    """
+    产品类别
+
+    Product class
+    """
+    FUTURES = "期货"
+    OPTION = "期权"
+
+
+class OrderType(Enum):
+    """
+    订单类型
+
+    Order type
+    """
+    LIMIT = "limit"     # 限价
+    MARKET = "market"   # 市价
+    STOP = "STOP"
+    FAK = "FAK"
+    FOK = "FOK"
+    RFQ = "询价"
+
+
+class OptionType(Enum):
+    """
+    期权类型
+
+    Option type.
+    """
+    CALL = "看涨期权"
+    PUT = "看跌期权"
+
+
+class Exchange(Enum):
+    """
+    交易所
+
+    Exchange
+    """
+    # Chinese
+    CFFEX = "CFFEX"         # China Financial Futures Exchange
+    SHFE = "SHFE"           # Shanghai Futures Exchange
+    CZCE = "CZCE"           # Zhengzhou Commodity Exchange
+    DCE = "DCE"             # Dalian Commodity Exchange
+    INE = "INE"             # Shanghai International Energy Exchange
+    GFEX = "GFEX"           # Guangzhou Futures Exchange
+    SSE = "SSE"             # Shanghai Stock Exchange
+    SZSE = "SZSE"           # Shenzhen Stock Exchange
+    BSE = "BSE"             # Beijing Stock Exchange
+    CFETS = "CFETS"         # CFETS Bond Market Maker Trading System
+
+
+class Currency(Enum):
+    """
+    货币
+
+    Currency
+    """
+    CNY = "CNY"
+
+
+class Interval(Enum):
+    """
+    数据间隔
+
+    Data interval
+    """
+    # 分钟
+    MINUTE = "1m"
+    THREE_MINUTE = "3m"
+    FIVE_MINUTE = "5m"
+    EIGHT_MINUTE = "8m"
+    THIRTEEN_MINUTE = "13m"
+    TWENTY_ONE_MINUTE = "21m"
+    THIRTY_FOUR_MINUTE = "34m"
+    FIFTY_FIVE_MINUTE = "55m"
+    EIGHTY_NINE_MINUTE = "89m"
+    ONE_HUNDRED_FORTY_FOUR_MINUTE = "144m"
+
+    HOUR = "1h"  # 小时
+    DAILY = "d"  # 日
+    WEEKLY = "w" # 周
