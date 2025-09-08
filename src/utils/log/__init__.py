3217893360b61396d7f8b73b0dbd8c1b76ed7330
Donny <donnymoving@gmail.com>
2025-09-08 17:13:56 +0800

    feat(utils/log): initialize logger in __init__.py
    - Create __init__.py file in log directory
    - Initialize logger for global use
    - Import and set up logger from logger module

---
+++ src/utils/log/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: Homalos
+@FileName   : __init__.py.py
+@Date       : 2025/9/8 15:43
+@Author     : Lumosylva
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: 日志默认初始化
+"""
+from src.utils.log.logger import init_logger
+# 全局 logger
+logger = init_logger()
