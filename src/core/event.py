24ddc44f7f8bcad3516a12422ca671453d550b6a
Donny <donnymoving@gmail.com>
2025-09-08 22:30:26 +0800

    feat(core): enhance Event class with additional attributes and functionality
    - Add uuid and typing imports for better data handling and unique identifier generation
    - Introduce new attributes: source and trace_id, to provide more context about the event
    - Implement __repr__ method for improved object representation
    - Create EventType enum for standardized event types

--- src/core/event.py
+++ src/core/event.py
@@ -9,7 +9,29 @@
 @Software   : PyCharm
 @Description: 事件类, 封装事件数据
 """
+import uuid
+from typing import Optional, Any
+
+
 class Event:
-    def __init__(self, event_type, **data):
-        self.event_type = event_type
-        self.data = data
+    def __init__(self, event_type, data: Any = None, source: Optional[str] = None, trace_id: Optional[str] = None):
+        self.event_type: str = event_type    # 事件类型
+        self.data: Any = data                # 事件数据
+        self.source: str = source or "unknown"          # 事件来源，如果没有提供来源，则默认为"unknown"
+        self.trace_id = trace_id or str(uuid.uuid4())   # 事件追踪ID，如果没有提供追踪ID，则生成一个新的UUID
+
+    def __repr__(self):
+        """
+        返回事件对象的字符串表示形式。
+        Args:
+            无。
+        Returns:
+            str: 事件对象的字符串表示形式，包含事件类型、事件源和追踪ID。
+        """
+        return f"Event(event_type={self.event_type}, source={self.source}, trace_id={self.trace_id})"
+
+
+class EventType:
+    """事件类型常量"""
+
+    EVENT_BUS_SHUTDOWN = "event_bus.shutdown"
