62ae789bcb692c069f9f0cf648f88c1a48129bc3
Donny <donnymoving@gmail.com>
2025-09-09 22:26:31 +0800

    feat):(constants add constants file for Homalos project
    - Create constants.py file with various project-related constants
    - Include project directory names, file names, and formatting constants
    - Add error code and reason mapping for better error handling

---
+++ src/constants.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: Homalos
+@FileName   : constants.py
+@Date       : 2025/9/9 17:10
+@Author     : Lumosylva
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: 常量
+"""
+# ================== 项目中目录名称 ==================
+PROJECT_NAME = "Homalos"  # 项目名称
+
+LOG_DIR_NAME = "log"  # 日志目录名
+
+DATA_DIR_NAME = "data"  # 数据目录名
+
+CONFIG_DIR_NAME = "config"  # 配置目录名
+
+# ================== 项目中文件名称 ==================
+BROKERS_FILENAME = "brokers.yaml"  # 多源服务器节点配置文件名
+
+LOG_CONFIG_FILENAME = "log_config.yaml"  # 全局日志配置文件名
+
+SYSTEM_DEV_CONFIG_FILENAME = "system.dev.yaml"
+
+SYSTEM_PROD_CONFIG_FILENAME = "system.prod.yaml"
+
+INSTRUMENT_EXCHANGE_FILENAME = "instrument_exchange.json"  # 期货合约与交易所映射信息文件名
+
+PRODUCT_INFO_FILENAME = "product_info.ini"  # 合约乘数及手续费信息文件名
+
+HOLIDAY_FILENAME = "holidays.json"  # 节假日文件名称
+
+
+# ================== 代码中常量 ==================
+file_format = "%Y%m%d"  # 日志文件名格式
+
+log_time_format = "%Y-%m-%d %H:%M:%S.%f"  # 日志文件中时间格式
+
+print_time_format = "%Y-%m-%d %H:%M:%S.%f"  # 控制台打印的时间格式
+
+# 错误码与错误原因映射
+REASON_MAPPING = {
+    0x1001: "网络读失败",
+    0x1002: "网络写失败",
+    0x2001: "接收心跳超时",
+    0x2002: "发送心跳失败",
+    0x2003: "收到错误报文"
+}
