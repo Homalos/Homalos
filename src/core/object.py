38d66b5de61524dfd930e43a63aa83972d908611
Donny <donnymoving@gmail.com>
2025-09-09 22:27:19 +0800

    feat(core): add new data classes for order, position, account and contract- Add OrderData class to represent order information - Add PositionData class to track position holdings - Add AccountData class to contain account balance and availability- Add ContractData class to store basic information of each trading contract - Add SubscribeRequest class for subscribing to tick data updates - Remove unused import of datetime - Update import to include OrderType, Direction, Offset and Product

--- src/core/object.py
+++ src/core/object.py
@@ -11,9 +11,7 @@
 """
 from dataclasses import dataclass, field
 
-from datetime import datetime as Datetime
-
-from src.core.constants import Exchange
+from src.core.constants import Exchange, OrderType, Direction, Offset, Product
 
 
 @dataclass
@@ -30,10 +28,10 @@ class BaseData:
 @dataclass
 class TickData(BaseData):
     """
-    报价数据包含以下信息：
+    tick报价数据包含以下信息：
         * 市场最新交易
         * 订单簿快照
-        * 日内市场统计数据。
+        * 日内市场统计数据
     """
     symbol: str = None
     exchange: Exchange = None
@@ -42,9 +40,74 @@ class TickData(BaseData):
 @dataclass
 class BarData(BaseData):
     """
-    特定交易周期的蜡烛图数据。
+    特定交易周期的蜡烛图数据
 
     Candlestick bar data of a certain trading period.
+    """
+    symbol: str = None
+    exchange: Exchange = None
+
+@dataclass
+class OrderData(BaseData):
+    """
+    订单数据
+    """
+    symbol: str = None
+    exchange: Exchange = None
+    order_id: str = None
+
+    type: OrderType = OrderType.LIMIT
+    direction: Direction | None = None
+    offset: Offset = Offset.NONE
+    price: float = 0
+    volume: float = 0
+    traded: float = 0
+
+
+@dataclass
+class PositionData(BaseData):
+    """
+    Position数据用于跟踪每个单独的位置持有情况。
+    Position data is used for tracking each individual position holding.
+    """
+    symbol: str = None
+    exchange: Exchange = None
+    direction: Direction = None
+
+    volume: float = 0
+    frozen: float = 0
+    price: float = 0
+    pnl: float = 0
+    yd_volume: float = 0
+
+@dataclass
+class AccountData(BaseData):
+    """
+    账户数据包含余额、冻结和可用信息。
+    Account data contains information about balance, frozen and
+    available.
+    """
+    account_id: str = None
+    balance: float = 0
+    frozen: float = 0
+
+@dataclass
+class ContractData(BaseData):
+    """
+    合约数据包含每份交易合约的基本信息。
+    """
+    symbol: str =  None
+    exchange: Exchange = None
+    name: str = None
+    product: Product = None
+    size: float = 0
+    price_tick: float = 0
+
+@dataclass
+class SubscribeRequest:
+    """
+    请求发送到特定网关以订阅报价数据更新。
+    Request sending to specific gateway for subscribing tick data update.
     """
     symbol: str = None
     exchange: Exchange = None
