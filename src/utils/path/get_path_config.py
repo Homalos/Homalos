809c622e1d7962f29c503f2d711300a7150a5853
Donny <donnymoving@gmail.com>
2025-09-10 22:18:59 +0800

    feat(utils): add path configuration utility- Create get_path_config.py file in utils/path directory - Define PROJECT_NAME constant for use in path operations

---
+++ src/utils/path/get_path_config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: Homalos
+@FileName   : get_path_config.py
+@Date       : 2025/9/10 22:10
+@Author     : Lumosylva
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: 路径工具配置
+"""
+PROJECT_NAME = "Homalos"
