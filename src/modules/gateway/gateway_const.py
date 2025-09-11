e5ffeb09232d571de649dde25cd803304ac2cce2
Donny <donnymoving@gmail.com>
2025-09-11 17:52:33 +0800

    refactor(gateway): update gateway constants and error handling
    - Import Status and ErrorReason from core constants
    - Update reason_mapping to use ErrorReason enum- Update order_status_names to use Status enum
    - Improve code readability and maintainability

--- src/modules/gateway/gateway_const.py
+++ src/modules/gateway/gateway_const.py
@@ -9,29 +9,36 @@
 @Software   : PyCharm
 @Description: 网关常量
 """
-from src.ctp.api.ctp_constant import THOST_FTDC_OST_Unknown, THOST_FTDC_OST_AllTraded, \
-    THOST_FTDC_OST_PartTradedQueueing, THOST_FTDC_OST_PartTradedNotQueueing, THOST_FTDC_OST_NoTradeQueueing, \
-    THOST_FTDC_OST_NoTradeNotQueueing, THOST_FTDC_OST_Canceled
+from src.core.constants import Status, ErrorReason
+from src.ctp.api.ctp_constant import (
+    THOST_FTDC_OST_Unknown,
+    THOST_FTDC_OST_AllTraded,
+    THOST_FTDC_OST_PartTradedQueueing,
+    THOST_FTDC_OST_PartTradedNotQueueing,
+    THOST_FTDC_OST_NoTradeQueueing,
+    THOST_FTDC_OST_NoTradeNotQueueing,
+    THOST_FTDC_OST_Canceled
+)
 
 
 class GatewayConst:
 
     # 错误码与错误原因映射
-    reason_mapping = {
-        0x1001: "网络读失败",
-        0x1002: "网络写失败",
-        0x2001: "接收心跳超时",
-        0x2002: "发送心跳失败",
-        0x2003: "收到错误报文"
+    reason_mapping: dict[str, ErrorReason] = {
+        "0x1001": ErrorReason.REASON_0x1001,
+        "0x1002": ErrorReason.REASON_0x1002,
+        "0x2001": ErrorReason.REASON_0x2001,
+        "0x2002": ErrorReason.REASON_0x2002,
+        "0x2003": ErrorReason.REASON_0x2003
     }
 
     # 订单状态常量映射  Order Status Constant Mapping
-    order_status_names = {
-        THOST_FTDC_OST_Unknown: "未知",
-        THOST_FTDC_OST_AllTraded: "全部成交",
-        THOST_FTDC_OST_PartTradedQueueing: "部分成交还在队列中",
-        THOST_FTDC_OST_PartTradedNotQueueing: "部分成交不在队列中",
-        THOST_FTDC_OST_NoTradeQueueing: "未成交还在队列中",
-        THOST_FTDC_OST_NoTradeNotQueueing: "未成交不在队列中",
-        THOST_FTDC_OST_Canceled: "撤单"
+    order_status_names: dict[str, Status] = {
+        THOST_FTDC_OST_Unknown: Status.UNKNOWN,
+        THOST_FTDC_OST_AllTraded: Status.ALL_TRADED,
+        THOST_FTDC_OST_PartTradedQueueing: Status.PART_TRADED_QUEUEING,
+        THOST_FTDC_OST_PartTradedNotQueueing: Status.PART_TRADED_NOT_QUEUEING,
+        THOST_FTDC_OST_NoTradeQueueing: Status.NO_TRADE_QUEUEING,
+        THOST_FTDC_OST_NoTradeNotQueueing: Status.NO_TRADE_NOT_QUEUEING,
+        THOST_FTDC_OST_Canceled: Status.CANCELED
     }
