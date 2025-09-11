b20fab8599cd5326bbabdc7049256c9740e5b86b
Donny <donnymoving@gmail.com>
2025-09-11 17:52:07 +0800

    feat(strategy): 重构策略基类并添加特定策略接口
    -重构 BaseStrategy 类，增加策略基本信息和通用方法
    - 新增 SpecificStrategyApi 类，提供特定策略接口
    - 添加 on_tick、on_bar

--- src/strategy/base_strategy.py
+++ src/strategy/base_strategy.py
@@ -9,14 +9,57 @@
 @Software   : PyCharm
 @Description: 策略基类
 """
-from src.utils.log import get_logger
+from src.core.object import TickData, BarData, OrderData, TradeData
+from src.strategy.strategy_function import check_on_tick, check_on_bar
 
 
-class BaseStrategy:
-    def __init__(self, name: str, subscribe_symbols: list):
-        self.name = name
-        self.subscribe_symbols = subscribe_symbols
-        self.logger = get_logger(context=f"Strategy-{name}")
+class BaseStrategy(object):
+    def __init__(self):
+        self.strategy_name = ""  # 策略名称
+        self.sub_ins_id = []  # 订阅的合约
+        self.sub_kline_type = []  # K线类型
+        self.strategy_content = ""  # 策略内容介绍
 
-    async def on_tick(self, tick: dict):
-        raise NotImplementedError
+    def one_min(self, now_time):
+        pass
+
+
+
+class SpecificStrategyApi(object):
+
+    def __init__(self, instrument_id: str, strategy_id: str, sub_kline_type: list):
+        self.instrument_id = instrument_id
+        self.strategy_id = strategy_id
+        self.sub_kline_type = sub_kline_type
+
+    def on_before_open(self):
+        """开盘前执行"""
+        pass
+
+    def on_after_close(self):
+        """收盘后执行"""
+        pass
+
+    def on_alarm(self):
+        """到达设置闹钟时间时执行"""
+        pass
+
+    @check_on_tick
+    def on_tick(self, tick: TickData):
+        """有新的tick产生时执行"""
+        # raise NotImplementedError
+        pass
+
+    @check_on_bar
+    def on_bar(self, bar: BarData):
+        """有新的Bar产生时执行"""
+        pass
+
+    def on_rtn_trade(self, trade: TradeData):
+        """有订单成交时执行"""
+        pass
+
+    def on_rtn_order(self, order: OrderData):
+        """订单状态发生改变时执行"""
+        pass
+
