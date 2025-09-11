2ec306e9cbed027d77f28234ea4fa773b10c5807
Donny <donnymoving@gmail.com>
2025-09-11 18:16:05 +0800

    refactor(strategy): 重构策略基类
    - 引入 ABC 和 abstractmethod，将 SpecificStrategyApi 类改为抽象基类
    - 为 BaseStrategy 类的属性添加类型注解
    - 为 SpecificStrategyApi 类的方法添加 abstractmethod 装饰器和类型提示
    - 删除 BaseStrategy 类中未使用的 one_min 方法

--- src/strategy/base_strategy.py
+++ src/strategy/base_strategy.py
@@ -9,57 +9,65 @@
 @Software   : PyCharm
 @Description: 策略基类
 """
+from abc import ABC, abstractmethod
+
 from src.core.object import TickData, BarData, OrderData, TradeData
 from src.strategy.strategy_function import check_on_tick, check_on_bar
 
 
 class BaseStrategy(object):
     def __init__(self):
-        self.strategy_name = ""  # 策略名称
-        self.sub_ins_id = []  # 订阅的合约
-        self.sub_kline_type = []  # K线类型
-        self.strategy_content = ""  # 策略内容介绍
+        self.strategy_id: int = 0
+        self.strategy_name: str = ""  # 策略名称
+        self.sub_ins_id: list = []  # 订阅的合约
+        self.sub_kline_type: list = []  # K线类型
+        self.strategy_content: str = ""  # 策略内容介绍
 
     def one_min(self, now_time):
         pass
 
 
-
-class SpecificStrategyApi(object):
+class SpecificStrategyApi(ABC):
 
     def __init__(self, instrument_id: str, strategy_id: str, sub_kline_type: list):
         self.instrument_id = instrument_id
         self.strategy_id = strategy_id
         self.sub_kline_type = sub_kline_type
 
-    def on_before_open(self):
+    @abstractmethod
+    def on_before_open(self) -> None:
         """开盘前执行"""
         pass
 
-    def on_after_close(self):
+    @abstractmethod
+    def on_after_close(self) -> None:
         """收盘后执行"""
         pass
 
-    def on_alarm(self):
+    @abstractmethod
+    def on_alarm(self) -> None:
         """到达设置闹钟时间时执行"""
         pass
 
     @check_on_tick
-    def on_tick(self, tick: TickData):
+    @abstractmethod
+    def on_tick(self, tick: TickData) -> None:
         """有新的tick产生时执行"""
-        # raise NotImplementedError
         pass
 
     @check_on_bar
-    def on_bar(self, bar: BarData):
+    @abstractmethod
+    def on_bar(self, bar: BarData) -> None:
         """有新的Bar产生时执行"""
         pass
 
-    def on_rtn_trade(self, trade: TradeData):
+    @abstractmethod
+    def on_rtn_trade(self, trade: TradeData) -> None:
         """有订单成交时执行"""
         pass
 
-    def on_rtn_order(self, order: OrderData):
+    @abstractmethod
+    def on_rtn_order(self, order: OrderData) -> None:
         """订单状态发生改变时执行"""
         pass
 
