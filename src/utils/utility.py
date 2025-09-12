53192b5ea6f660c4c81829fbc23d2457cf09a774
Donny <donnymoving@gmail.com>
2025-09-12 23:50:39 +0800

    feat(utils): add functions for INI file handling and improve utility functions
    - Add load_ini function to read INI configuration files
    - Add write_ini function to write configuration to INI files- Implement del_num function to remove digits from a string
    - Update load_json function to include error handling
    - Refactor load_config function for better error handling

--- src/utils/utility.py
+++ src/utils/utility.py
@@ -9,8 +9,10 @@
 @Software   : PyCharm
 @Description: 公用的工具
 """
+import configparser
 import json
 import os
+import re
 from typing import Any
 
 import yaml
@@ -18,6 +20,7 @@ import yaml
 from src.utils.log import get_logger
 
 _logger = get_logger(__name__)
+
 
 def load_json(file_path: str) -> dict[str, Any]:
     """
@@ -51,8 +54,48 @@ def write_json(file_path: str, data: dict[str, Any]) -> None:
     except IOError as e:
         _logger.error("无法写入文件 {}: {}".format(file_path, e))
 
+def load_ini(file_path: str) -> configparser:
+    """
+    从指定路径加载INI配置文件。
+
+    Args:
+        file_path (str): INI配置文件的路径。
+
+    Returns:
+        ConfigParser: 加载的INI配置文件对象。
+
+    说明：
+        如果指定的文件不存在，则会创建一个空的INI文件。
+        如果文件存在，则读取文件内容并返回一个ConfigParser对象。
+        在创建空文件时，可以选择写入一些默认的空section或者注释，如果需要的话。
+
+    """
+    config_parser: configparser = configparser.ConfigParser()
+    # 检查文件是否存在，如果不存在则创建一个空的ini文件
+    if not os.path.exists(file_path):
+        with open(file_path, 'w', encoding='utf-8') as f:
+            f.write("")
+    config_parser.read(file_path, encoding='utf-8')
+
+    return config_parser
+
+def write_ini(config_parser: configparser, file_path: str) -> None:
+    """
+    将配置写入ini文件。
+
+    Args:
+        config_parser (ConfigParser): 配置文件对象。
+        file_path (str): 要写入的ini文件路径。
+
+    """
+    config_parser.write(open(file_path, "w", encoding='utf-8'))
+
 def load_config(config_path: str) -> dict:
-    """加载配置文件"""
+    """
+    加载 yaml 配置文件
+    :param config_path:
+    :return:
+    """
     if not os.path.exists(config_path):
         _logger.error(f"未找到配置文件: {config_path}")
         raise FileNotFoundError(f"配置文件不存在: {config_path}")
@@ -70,4 +113,16 @@ def prepare_address(address: str) -> str:
     if not any(address.startswith(scheme) for scheme in ["tcp://", "ssl://", "socks://"]):
         return "tcp://" + address
     return address
+
+def del_num(content) -> str:
+    """
+    删除字符串中的所有数字。
+
+    Args:
+        content (str): 需要删除数字的字符串。
+
+    Returns:
+        str: 删除数字后的字符串。
+    """
+    return re.sub(r'\d', '', content)
 
