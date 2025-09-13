45c54329ce022b8d9d1099981a950f158d1b4201
Donny <donnymoving@gmail.com>
2025-09-13 22:29:37 +0800

    feat(module): add initialization files for history_data and market_data
    - Create __init__.py files for history_data and market_data modules
    - Set up basic file structure and metadata for the new modules

---
+++ src/modules/history_data/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: Homalos
+@FileName   : __init__.py.py
+@Date       : 2025/9/13 20:52
+@Author     : Lumosylva
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: description
+"""
