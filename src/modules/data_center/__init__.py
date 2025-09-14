ea167323616c84346fa4cf5a9064b808d59e74e1
Donny <donnymoving@gmail.com>
2025-09-14 21:34:30 +0800

    refactor(modules): rename history_data to data_center
    - Rename history_data module to data_center
    - Update __init__.py file to reflect new module name
    - Modify description from 'description' to '数据中心初始化'

--- src/modules/data_center/__init__.py
+++ src/modules/data_center/__init__.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 """
 @ProjectName: Homalos
-@FileName   : __init__.py.py
+@FileName   : __init__.py
 @Date       : 2025/9/13 20:52
 @Author     : Lumosylva
 @Email      : donnymoving@gmail.com
 @Software   : PyCharm
-@Description: description
+@Description: 数据中心初始化
 """
