d11804d3385cda8e255508e9335c1d6c9a8556bd
Donny <donnymoving@gmail.com>
2025-09-24 22:59:36 +0800

    feat(strategy): 添加全局策略模块
    - 创建全局策略文件 global_strategy.py
    - 添加文件头部注释信息
    - 初始化全局策略相关配置

---
+++ src/strategy/global_strategy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: Homalos
+@FileName   : global_strategy.py
+@Date       : 2025/9/21 22:19
+@Author     : Lumosylva
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: 全局策略
+"""
+
