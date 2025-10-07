c52ebc0f6cd0396c4971c383f9e814d6ae5218c7
Donny <donnymoving@gmail.com>
2025-10-07 23:37:02 +0800

    feat(thread_pool): initialize thread pool instance
    - Create thread pool module with basic initialization
    - Implement ThreadPool class instantiation- Add module documentation and metadata
    - Set up thread pool instance for application use

---
+++ src/api/thread_pool/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: Homalos
+@FileName   : __init__.py.py
+@Date       : 2025/10/7 21:02
+@Author     : Lumosylva
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: 初始化线程池实例
+"""
+from src.api.thread_pool.thread_pool import ThreadPool
+
+thread_pool_ins = ThreadPool()
