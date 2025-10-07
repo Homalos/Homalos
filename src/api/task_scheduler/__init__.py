cb574cb67c9384f05132df4de6f9f028b704dfe0
Donny <donnymoving@gmail.com>
2025-10-07 23:36:51 +0800

    feat(task_scheduler): initialize task scheduler module- Create task scheduler initialization file - Import and instantiate TaskScheduler class - Add module metadata and author information

---
+++ src/api/task_scheduler/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: Homalos
+@FileName   : __init__.py.py
+@Date       : 2025/10/7 20:32
+@Author     : Lumosylva
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: 初始化任务调度器
+"""
+from src.api.task_scheduler.task_scheduler import TaskScheduler
+
+task_scheduler_ins = TaskScheduler()
