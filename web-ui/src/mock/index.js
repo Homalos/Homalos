5b3e8cd8056068998e146837ddf89648cba02cec
Donny <donnymoving@gmail.com>
2025-10-10 00:22:12 +0800

    feat(mock): add mock data export module- Create unified export file for all mock data - Export strategy templates mock data - Export strategy logs mock data - Export scheduled tasks mock data- Export notifications mock data - Export console logs mock data- Export strategies mock data - Export dashboard mock data

---
+++ web-ui/src/mock/index.js
@@ -0,0 +1,12 @@
+/**
+ * Mock 数据统一导出
+ */
+
+export { strategyTemplates } from './strategyTemplates'
+export { strategyLogsData } from './strategyLogs'
+export { scheduledTasksData } from './scheduledTasks'
+export { notificationsData } from './notifications'
+export { consoleLogsData } from './consoleLogs'
+export { strategiesData } from './strategies'
+export { dashboardData } from './dashboardData'
+
