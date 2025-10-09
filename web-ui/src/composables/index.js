a778d3f6f01fc52723ae823e7f70e0e8889a5350
Donny <donnymoving@gmail.com>
2025-10-10 00:53:29 +0800

    feat(composables): add unified export for composables
    - Export useSystemMonitor composable
    - Export useStrategyManagement composable
    - Export useTaskScheduler composable
    - Export useNotifications composable- Export useConsole composable
    - Create index.js for composables unified export

---
+++ web-ui/src/composables/index.js
@@ -0,0 +1,10 @@
+/**
+ * Composables 统一导出
+ */
+
+export { useSystemMonitor } from './useSystemMonitor'
+export { useStrategyManagement } from './useStrategyManagement'
+export { useTaskScheduler } from './useTaskScheduler'
+export { useNotifications } from './useNotifications'
+export { useConsole } from './useConsole'
+
