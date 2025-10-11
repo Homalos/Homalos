4e624534f6c172dfaf826286531a8f37735759aa
Donny <donnymoving@gmail.com>
2025-10-11 17:51:37 +0800

    refactor(strategy): rename module and update initialization
    - Renamed module from 'function' to 'strategy1'
    - Updated file path to reflect new module structure- Added Strategy1 class import and instantiation
    - Updated file header date and maintained author information

--- src/strategy/strategy1/__init__.py
+++ src/strategy/strategy1/__init__.py
@@ -3,9 +3,12 @@
 """
 @ProjectName: Homalos
 @FileName   : __init__.py.py
-@Date       : 2025/9/15 14:12
+@Date       : 2025/10/11 15:42
 @Author     : Lumosylva
 @Email      : donnymoving@gmail.com
 @Software   : PyCharm
 @Description: description
 """
+from src.strategy.strategy1.strategy1 import Strategy1
+
+strategy1 = Strategy1()
