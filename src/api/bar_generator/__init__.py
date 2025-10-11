3be4f954c5b07b480e7b941d03c9c4bbe31c092e
Donny <donnymoving@gmail.com>
2025-10-11 17:51:03 +0800

    refactor(api): restructure bar generator module
    - Rename module from function.py to __init__.py- Update file header information including filename and date
    - Modify description field in module docstring
    - Import BarGenerator class from bar_generator module
    - Instantiate BarGenerator as bar_generator_ins

---
+++ src/api/bar_generator/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: Homalos
+@FileName   : __init__.py.py
+@Date       : 2025/10/11 16:01
+@Author     : Lumosylva
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: description
+"""
+from src.api.bar_generator.bar_generator import BarGenerator
+
+bar_generator_ins = BarGenerator()
