ef75a15d29f80b527b29eea8d9745e5ea7c99b48
Donny <donnymoving@gmail.com>
2025-10-11 17:51:26 +0800

    feat(function): add function instance initialization
    - Created __init__.py file for function module- Imported Function class from src.function.function
    - Instantiated Function class as function_ins
    - Added module documentation header with project details

---
+++ src/function/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: Homalos
+@FileName   : __init__.py.py
+@Date       : 2025/10/11 16:18
+@Author     : Lumosylva
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: Function实例
+"""
+from src.function.function import Function
+
+function_ins = Function()
