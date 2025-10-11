0596f2919e1f5e5fb2003ad43d974695b279d18c
Donny <donnymoving@gmail.com>
2025-10-11 15:35:09 +0800

    refactor(core): rename main.py to start_homalos.py
    - Moved main function to start_homalos.py
    - Added file header with project information
    - Preserved existing functionality and structure

---
+++ start_homalos.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: Homalos
+@FileName   : start_homalos.py
+@Date       : 2025/10/11 15:34
+@Author     : Lumosylva
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: 系统入口
+"""
+def main():
+    print("Hello from homalos!")
+
+
+if __name__ == "__main__":
+    main()
