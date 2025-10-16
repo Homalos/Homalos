fe17bc93d8d22cd7a140629b3e82359f33a25a97
Donny <donnymoving@gmail.com>
2025-10-16 15:28:07 +0800

    refactor(strategy): rename strategy1 module to strategies
    - Renamed module directory from 'strategy1' to 'strategies'
    - Updated file paths accordingly- Removed outdated strategy1 import and instantiation
    - Updated author date in module header

--- src/strategy/strategies/__init__.py
+++ src/strategy/strategies/__init__.py
@@ -3,12 +3,9 @@
 """
 @ProjectName: Homalos
 @FileName   : __init__.py.py
-@Date       : 2025/10/11 15:42
+@Date       : 2025/10/16 10:37
 @Author     : Lumosylva
 @Email      : donnymoving@gmail.com
 @Software   : PyCharm
 @Description: description
 """
-from src.strategy.strategy1.strategy1 import Strategy1
-
-strategy1 = Strategy1()
