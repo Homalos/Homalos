f324764d9c92b5df899e6dd2937d74a0fb9c74f2
Donny <donnymoving@gmail.com>
2025-10-16 15:31:17 +0800

    refactor(strategy): rename strategy_function.py to decorators.py
    - Renamed file from `strategy_function.py` to `decorators.py`
    - Added `subscribe` decorator for event subscription in strategies
    - Retained existing `check_on_tick` and `check_on_bar` functions
    - Updated file header information and descriptions
    - Removed unnecessary blank lines and comments

--- src/strategy/decorators.py
+++ src/strategy/decorators.py
@@ -2,19 +2,38 @@
 # -*- coding: utf-8 -*-
 """
 @ProjectName: Homalos
-@FileName   : strategy_function.py
-@Date       : 2025/9/11 17:41
+@FileName   : decorators.py
+@Date       : 2025/10/16 10:24
 @Author     : Lumosylva
 @Email      : donnymoving@gmail.com
 @Software   : PyCharm
-@Description: description
+@Description: subscribe 装饰器
+
+src/strategy/decorators.py
 """
 import time
-
+from typing import Callable
 from src.utils.log import get_logger
 
-
 _logger = get_logger(__name__)
+
+
+def subscribe(event_type: str, async_mode: bool = False):
+    """
+    装饰器：标记策略函数订阅的事件类型
+
+    策略函数打上此装饰器后，系统会自动识别并注册到 EventBus。
+
+    使用示例：
+        @subscribe(EventType.TICK)
+        def on_tick(event): ...
+    """
+    def decorator(func: Callable):
+        if not hasattr(func, "_subscribe_to"):
+            func._subscribe_to = []
+        func._subscribe_to.append((event_type, async_mode))
+        return func
+    return decorator
 
 def check_on_tick(func):
     def checkfunc(self, *args, **kwargs):
@@ -24,11 +43,7 @@ def check_on_tick(func):
         spend_time = end - start
         if spend_time > 0.5:
             _logger.warning('策略{}中的{}合约的{}函数花费{}s，超出规定时间，请注意！'.format(self.strategy_id, self.instrument_id, func.__name__, spend_time))
-
-        # self.market_data_lock.release()
-
     return checkfunc
-
 
 def check_on_bar(func):
     def checkfunc(self, *args, **kwargs):
@@ -39,7 +54,5 @@ def check_on_bar(func):
         spend_time = end - start
         if spend_time > 1:
             _logger.warning('策略{}中的{}合约的{}函数花费{}s，超出规定时间，请注意！'.format(self.strategy_id, self.instrument_id, func.__name__, spend_time))
-
         self.kline_lock.release()
-
     return checkfunc
